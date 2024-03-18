e38e4f751c2b1b9696f5c24e98af3ea2639d8bb6
Varren999 <varren999@gmail.com>
2024-03-18 15:27:14 +0700

    Display

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DesktopMonitor"))
+            {
+                foreach (ManagementObject currentObj in searcher.Get())
+                {
+                    Console.WriteLine("----------- Win32_VideoController instance -------");
+                    Console.WriteLine("AdapterRAM: {0}", item["AdapterRAM"]);
+                    Console.WriteLine("Caption: {0}", item["Caption"].ToString());
+                    Console.WriteLine("Description: {0}", item["Description"].ToString());
+                    Console.WriteLine("VideoProcessor: {0}", item["VideoProcessor"].ToString());
+                    string Adapter = currentObj["AdapterRam"].ToString();
+                    Console.WriteLine(Adapter);
+                }
+            }
+        }
+    }
+}
