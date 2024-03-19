c8b9ec4d80e1d042b9497d357b9ea46eb11c55c8
Varren999 <varren999@gmail.com>
2024-03-19 19:58:50 +0700

    work version

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,29 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Management;
 
-namespace ConsoleApp6
+namespace DisplayInfo
 {
-    internal class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DesktopMonitor"))
-            {
-                foreach (ManagementObject currentObj in searcher.Get())
-                {
-                    Console.WriteLine("----------- Win32_VideoController instance -------");
-                    Console.WriteLine("AdapterRAM: {0}", item["AdapterRAM"]);
-                    Console.WriteLine("Caption: {0}", item["Caption"].ToString());
-                    Console.WriteLine("Description: {0}", item["Description"].ToString());
-                    Console.WriteLine("VideoProcessor: {0}", item["VideoProcessor"].ToString());
-                    string Adapter = currentObj["AdapterRam"].ToString();
-                    Console.WriteLine(Adapter);
-                }
-            }
+            ScreenSize screen = new ScreenSize();
+            Console.WriteLine("Диагональ экрана: {0:F2} дюймов", screen.Size());
         }
     }
 }
---
+++ ConsoleApp6/ScreenSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DisplayInfo
+{
+    internal class ScreenSize
+    {
+        private readonly double widthInInches, heightInInches;
+        public ScreenSize() 
+        {
+            foreach (var screen in Screen.AllScreens)
+            {
+                if(screen.Primary)
+                {
+                    widthInInches = screen.Bounds.Width / 96.0;
+                    heightInInches = screen.Bounds.Height / 96.0;
+                }               
+            }
+        }
+        public double Size() => Math.Sqrt(Math.Pow(widthInInches, 2) + Math.Pow(heightInInches, 2));
+    }
+}
